54fcf459e35c3c839418e066b612e948f8c2f89f
p3nguin <p3nguinkun@proton.me>
2024-02-27 22:05:40 +0700

    disable auto formatting

--- main/.config/nvim/lua/plugins/lsp/null-ls.lua
+++ main/.config/nvim/lua/plugins/lsp/null-ls.lua
@@ -3,31 +3,10 @@ if not setup then
     return
 end
 
-local formatting = null_ls.builtins.formatting   -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
 null_ls.setup({
     debug = true,
-    sources = {
-        -- Webdev stuff
-        formatting.deno_fmt.with({ extra_args = { "--style", "{IndentWidth: 4}" } }),
-        formatting.prettier.with({ extra_args = { "--style", "{IndentWidth: 4}" } }),
-
-        -- Cpp, C with indent width 4
-        formatting.clang_format.with({ extra_args = { "--style", "{IndentWidth: 4}" } }),
-
-        -- Python
-        formatting.black.with({ extra_args = { "--style", "{IndentWidth: 4}" } }),
-
-        -- JS/TS Linter
-        diagnostics.eslint_d.with({
-            condition = function(utils)
-                return utils.root_has_file(".eslintrc.js")
-            end,
-        }),
-        -- Gitsigns
-        null_ls.builtins.code_actions.gitsigns,
-    },
     -- configure format on save
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
